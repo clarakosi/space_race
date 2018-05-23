54ca3f93d1e650e337b04705e0ac684fc9392bb9
Clara Andrew <candrewwani@gmail.com>
2018-05-23 17:53:39 -0400

    models draft

--- teams/models.py
+++ teams/models.py
@@ -2,8 +2,33 @@ from uuid import uuid4
 from django.contrib.auth.models import User
 from django.db import models
 
-class Team(models.Model):
-    title = models.CharField(max_length=20)
+# class Team(models.Model):
+#     title = models.CharField(max_length=20)
 
-    def __str__(self):
-        return self.title
+#     def __str__(self):
+#         return self.title
+
+class Student(model.Model):
+    name = models.CharField(max_length=None)
+
+class Team(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_lengh=200)
+    students = models.ForeignKey(Student)
+    score = models.IntegerField(default=0)
+
+class Quiz(models.Model):
+    name = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    teams = models.ForeignKey(Team)
+    created_at = models.DateTimeField(auto_now=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz)
+    question = models.CharField(max_length=None)
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question)
+    answer = models.CharField(max_lengh=None)
+    is_correct = models.BooleanField(initial=False)
--- teams/serializers.py
+++ teams/serializers.py
@@ -1,11 +1,33 @@
 from rest_framework import serializers
-from . import models
+from .models import Team, Quiz
 
-class TeamSerializer(serializers.ModelSerializer):
+# class TeamSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         fields = (
+#             'id',
+#             'title',
+#         )
+#         model = models.Team
+
+class TeamSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        fields = (
-            'id',
-            'title',
-        )
-        model = models.Team
-        
+        model = Team
+        fields = ('name')
+    
+    def create(self, validated_data):
+        """Override create to associate current user with Team creator"""
+        user = self.context['request'].user
+        team = Team.objects.create(user=user, **validated_data)
+        return team
+
+class QuizSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Quiz
+        fields = ('name')
+
+    def create(self, validated_data):
+    """Override create to associate current user with Quiz creator"""
+    user = self.context['request'].user
+    quiz = Quiz.objects.create(user=user, **validated_data)
+    return quiz  
+
