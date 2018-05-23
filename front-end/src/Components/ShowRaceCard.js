8de1a6018a871447c93afd31d87f98661cfb6a19
FJF616 <franzjfernando@hotmail.com>
2018-05-23 15:12:37 -0700

    adds random teams radio button, edit icon, trashicon, and placeholder for team icons.

--- front-end/src/Components/ShowRaceCard.js
+++ front-end/src/Components/ShowRaceCard.js
@@ -10,6 +10,7 @@ const ShowRaceCard = React.createClass({
           <title>showRaceCard</title>
           <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
           <link rel="stylesheet" href="assets/fonts/font-awesome.min.css" />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" />
           <link rel="stylesheet" href="assets/css/styles.css" />
           <div className="card border-dark" style={{backgroundColor: 'rgba(189,245,252,0.2)'}}>
             <div className="card-header" style={{height: 55}}>
@@ -31,19 +32,22 @@ const ShowRaceCard = React.createClass({
             </div>
             <div className="row no-gutters" style={{width: 1000, margin: 0}}>
               <div className="col" style={{margin: 10}}>
-                <h3 className="text-left bg-info align-content-center m-auto">Teams</h3>
+                <h3 className="text-left bg-info align-content-center m-auto" style={{padding: 10}}>Teams</h3>
               </div>
             </div>
             <div className="row m-auto" style={{width: 1000, margin: 0}}>
               <div className="col-auto justify-content-center align-content-center" style={{margin: 10}}><input className="form-control-plaintext order-1" type="text" defaultValue="team name" readOnly style={{fontSize: 20}} /></div>
               <div className="col-auto" style={{margin: 10}}><input className="form-control-plaintext order-1" type="text" defaultValue="Color(chosen from above)" readOnly style={{fontSize: 15, padding: 0, margin: 10}} /></div>
-              <div className="col-auto" style={{margin: 10}}><button className="btn btn-primary" type="button"><i className="fa fa-trash-o" /></button></div>
+              <div className="col-auto align-items-center align-self-center"><i className="fa fa-pencil" data-bs-hover-animate="bounce" /></div>
+              <div className="col-auto justify-content-center align-items-center align-self-center" style={{margin: 10}}><i className="fa fa-trash-o" /></div>
               <div className="col-auto align-self-center"><i className="fa fa-star d-table-cell d-inline-flex justify-content-center align-items-center m-auto" /></div>
             </div>
             <div className="card-body">
               <div className="row">
-                <div className="col m-auto"><a className="btn btn-outline-primary" role="button" href="#Questions" style={{width: 225}}>On To Questions &nbsp;<i className="fa fa-space-shuttle" /></a></div>
-                <div className="col" />
+                <div className="col-auto"><a className="btn btn-outline-primary" role="button" href="#Questions" style={{width: 225}}>On To Questions &nbsp;<i className="fa fa-space-shuttle" /></a></div>
+                <div className="col-auto">
+                  <div className="form-check"><input className="form-check-input" type="radio" id="formCheck-1" /><label className="form-check-label" htmlFor="formCheck-1">Random Teams</label></div>
+                </div>
               </div>
             </div>
           </div>
