cc444edb12ba35a2330bb284806055b52ed4f5b1
Capril1042 <Caprilewis1042@gmail.com>
2018-05-24 13:57:11 -0400

    SignUp and SignIn Components- Skeleton Only NON-Functional

--- src/Components/UserAccounts/SignIn.js
+++ src/Components/UserAccounts/SignIn.js
@@ -1,5 +1,5 @@
 import React, { Componenet } from 'react';
-import{ Button, Link } from 'reactstrap';
+import{ Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 class SignIn extends Component {
     constructor(props) {
@@ -11,21 +11,23 @@ class SignIn extends Component {
     }
 
     handleSubmit(){
-        
+
     }
     
     render() {
         return(
-            <div>
-                <form onSumbit={this.handleSubmit}>
-                    <h2> Sign-In to your Space Race Account</h2>
-                    <input class="form-control" type="email" name="email" placeholder="Email"/>
-                    <input class="form-control" type="password" name="password" placeholder="Password"/>
-                    <Button onClick={this.handleSubmit}>Sign In </Button>
+            <Form>
+                <h2> Sign-In to your Space Race Account</h2>
+                <FormGroup>
+                    <Input type="email" name="email" placeholder="Email"/>
+                </FormGroup>
+                <FormGroup>
+                    <Input type="password" name="password" placeholder="Password"/>
+                </FormGroup>
+                <Button onClick={this.handleSubmit}>Sign In </Button>
                     <h6> Don't Already have an Account?</h6>
                     <Link to="/SignUp"> Sign Up Today </Link>
-                </form>
-            </div>
+            </Form>
         );
     }
 }
--- src/Components/UserAccounts/SignUp.js
+++ src/Components/UserAccounts/SignUp.js
@@ -1,13 +1,53 @@
 import React, { Componenet } from 'react';
+import{ Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 class SignUp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        };
+    }
 
+    handleSubmit(){
+        
+    }
+    
     render() {
         return(
-        <div>
-        </div>
+            <Form>
+                <h2> Create a Space Race Account</h2>
+                <FormGroup>
+                    <Input  type="email" name="email" placeholder="Email"/>
+                </FormGroup>
+                <FormGroup>
+                    <Input type="password" name="password" placeholder="Password"/>
+                </FormGroup>
+                <FormGroup>
+                    <Input type="password" name="password" placeholder="Password(repeat)"/>
+                </FormGroup>
+                <FormGroup tag="fieldset">
+                    <legend>Account Type</legend>
+                    <FormGroup check>
+                    <Label check>
+                        <Input type="radio" name="radio1" />{' '}
+                        I'm Signing Up for a Teacher Account
+                    </Label>
+                </FormGroup>
+                <FormGroup check>
+                <Label check>
+                  <Input type="radio" name="radio1" />{' '}
+                        I'm Signing Up for a Student Account
+                </Label>
+                </FormGroup>
+                </FormGroup>
+                    <Button onClick={this.handleSubmit}>Sign Up </Button>
+                    <h6> Already have an Account?</h6>
+                    <Link to="/SignIn"> Sign In </Link>
+            </Form>
+    
         );
     }
 }
-
 export default SignUp;
