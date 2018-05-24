c2df2475295850b02c2a5c6973f8a543eb57fd8d
Clara Andrew <candrewwani@gmail.com>
2018-05-24 18:11:20 -0400

    merge conflict fixed

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { combineReducers } from 'redux';
 import ShuffleReducer from './ShuffleArrayReducer';
 
@@ -7,13 +6,3 @@ const RootReducers = combinReducers({
 })
 
 export default RootReducers;
-=======
-const rootReducer = (state=[], action) => {
-  switch(action.type) {
-    default:
-      return state
-  }
-}
-
-export default rootReducer;
->>>>>>> 8911845d95c4d100e9d5092bc1d6a1950bb56619
