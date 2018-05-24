56c367be399000533bad32fd3ae7b60cfffa7737
FJF616 <franzjfernando@hotmail.com>
2018-05-24 15:51:31 -0700

    saving progress on race card view

--- src/Components/CardViews/ShowRaceCard.js
+++ src/Components/CardViews/ShowRaceCard.js
@@ -10,14 +10,26 @@ class ShowRaceCard extends Component {
     constructor (props) {
         super(props);
         this.state = {
-            checked: false
+            checked: false,
+            teams:[]
         };
         this.toggleCheck =  this.toggleCheck.bind(this);
+        this.handleClick= this.handleClick.bind(this);
+        this.handleSubmit=this.handleSubmit.bind(this);
     }
+    handleChange(changeEvent) {
+      this.setState({
+        selectedOption: changeEvent.target.value
+      });
+    },
+    
     toggleCheck() {
       this.setState({
         checked: !this.state.checked
       });
+    }
+    handleSubmit(event){
+      event.preventDefault();
     }
     render() {
       return (
@@ -38,19 +50,21 @@ class ShowRaceCard extends Component {
                 </div>
               </div>
             </div>
-            <div className="col-auto"><input type="text" placeholder="Race Name" /></div>
+            <form onSubmit={this.handleSubmit}>
+            <div className="col-auto"><input type="text" placeholder="Race Name" onChange={this.handleChange}/></div>
             <h5 className="text-left bg-info" style={{margin: 10}}>Add a Team</h5>
             <div className="row my-auto visible" style={{margin: '-4px'}}>
               <div className="col-auto m-auto"><select><optgroup label="Select Team"><option value="Slytherin">Slytherin</option><option value="Griffyndor">Griffyndor</option><option value="Hufflepuff">Hufflepuff</option><option value="Ravenclaw">RavenClaw</option></optgroup></select></div>
-              <div className="col-auto m-auto"><input type="color" /></div>
+              <div className="col-auto m-auto"><input type="color"/></div>
               <div className="col-auto m-auto"><select><optgroup label="Select Spaceship"><option value={12} selected>This is item 1</option><option value={13}>This is item 2</option><option value={14}>This is item 3</option></optgroup></select></div>
-              <div className="col"><button className="btn btn-primary btn-lg float-right" type="submit" style={{fontSize: 10, padding: 0, width: 55, margin: '15px 12px', height: 25}} onClick={()=>this.handleClick}>ADD</button></div>
-            </div>
+              <div className="col"><button className="btn btn-primary btn-lg float-right" type="submit" style={{fontSize: 10, padding: 0, width: 55, margin: '15px 12px', height: 25}}>ADD</button></div></div>        
             <div className="row no-gutters" style={{width: 1000, margin: 0}}>
               <div className="col" style={{margin: 10}}>
                 <h3 className="text-left bg-info align-content-center m-auto" style={{padding: 10}}>Teams</h3>
               </div>
+           
             </div>
+           </form>
             <div className="row m-auto" style={{width: 1000, margin: 0}}>
               <div className="col-auto justify-content-center align-content-center" style={{margin: 10}}><input className="form-control-plaintext order-1" type="text" defaultValue="team name" readOnly style={{fontSize: 20}} /></div>
               <div className="col-auto" style={{margin: 10}}><input className="form-control-plaintext order-1" type="text" defaultValue="Color(chosen from above)" readOnly style={{fontSize: 15, padding: 0, margin: 10}} /></div>
@@ -62,7 +76,7 @@ class ShowRaceCard extends Component {
               <div className="row">
                 <div className="col-auto"><a className="btn btn-outline-primary" role="button" href="#Questions" style={{width: 225}}>On To Questions &nbsp;<i className="fa fa-space-shuttle" /></a></div>
                 <div className="col-auto">
-                  <div className="form-check"><input className="form-check-input" type="radio" id="formCheck-1" /><label className="form-check-label" htmlFor="formCheck-1">Random Teams</label></div>
+                  <div className="form-check"><input className="form-check-input" type="radio" id="formCheck-1"  onChange={this.toggleCheck} checked={this.state.checked}/><label className="form-check-label" htmlFor="formCheck-1">Random Teams</label></div>
                 </div>
               </div>
             </div>
