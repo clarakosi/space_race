f6d5a413c9dc0637b2f9913e21643824e8bb5fae
FJF616 <franzjfernando@hotmail.com>
2018-05-25 08:49:58 -0700

    adds error condition to array random array reducer

--- src/Reducers/ShuffleArrayReducer.js
+++ src/Reducers/ShuffleArrayReducer.js
@@ -5,15 +5,19 @@ export default (state = initialState, action) => {
     switch (action.type) {
         case SHUFFLE_ARRAY: 
             const array = action.payload
+            if (array.length > 0) {
             let i = array.length - 1;
-            for (; i > 0; i--) {
-                const j = Math.floor(Math.random() * (i + 1));
-                const temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-            }
-            return [...array];
-            default: return state;
+                for (; i > 0; i--) {
+                    const j = Math.floor(Math.random() * (i + 1));
+                    const temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    
+                }
+                return [...array];
+            } else { alert("There are no teams scheduled for this race.") }
+        default : return state
+            
     }
     
 }
