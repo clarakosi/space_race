a95a4694c3ddae1f719dcec58cd2b0b8871b186f
KickMe <kickme@alifs-mbp.gateway.sonic.net>
2018-05-29 16:42:16 -0700

    tokenization still buggy - working on 2 errors I keep getting

--- backend/settings.py
+++ backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 3rd party apps
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
     # Our apps
     'teams',
@@ -138,11 +139,22 @@ STATICFILES_DIRS = (
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
+
+""" REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
+        'rest_framework.authentication.TokenAuthentication',
     ]
-}
+} """
 
 CORS_ORIGIN_WHITELIST = (
     config('CORS')
--- backend/urls.py
+++ backend/urls.py
@@ -13,24 +13,38 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
+from django.conf.urls import url
+from django.urls import include, path
+from accounts.views import UserCreate
 
+urlpatterns = [
+    url(r'^$', UserCreate.as_view(), name='account-create'),
+     path('admin/', admin.site.urls),
+    path('api/', include('teams.urls')),
+  # I think this is what was causing the NoReverseMatch error?  
+  # url(r'^users/', include('accounts.urls')),
+    url(r'^/', include('accounts.urls')),
+]
+
+
+
+
+
+
+"""
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls import include, url
 from django.views.generic import TemplateView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('teams.urls')),
+  # I think this is what was causing the NoReverseMatch error?  
   # url(r'^users/', include('accounts.urls')),
     url(r'^/', include('accounts.urls')),
     url('.*', TemplateView.as_view(template_name='index.html')),
 ]
-
 """
-from django.conf.urls import include, url
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^/', include('accounts.urls')),
-]"""
