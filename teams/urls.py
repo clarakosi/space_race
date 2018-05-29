a58252d6ff6c3ca9edb53d7ede59f165be05de6d
Clara Andrew <candrewwani@gmail.com>
2018-05-29 12:46:03 -0400

    model restructered wip

--- teams/urls.py
+++ teams/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.ListQuiz.as_view()),
     path('<int:pk>/', views.DetailQuiz.as_view()),
+    path('questions/', views.ListQuestion.as_view()),
+    path('questions/<int:pk>', views.DetailQuestion.as_view())
 ]
--- teams/views.py
+++ teams/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 
-from .models import Quiz
-from .serializers import QuizSerializer
+from .models import Quiz, Question
+from .serializers import QuizSerializer, QuestionSerializer
 
 class ListQuiz(generics.ListCreateAPIView):
     queryset = Quiz.objects.all()
@@ -10,3 +10,11 @@ class ListQuiz(generics.ListCreateAPIView):
 class DetailQuiz(generics.RetrieveUpdateDestroyAPIView):
     queryset = Quiz.objects.all()
     serializer_class = QuizSerializer
+
+class ListQuestion(generics.ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+class DetailQuestion(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
