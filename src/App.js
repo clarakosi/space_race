fd4c20ac45bad01b97256376c4ff7b4d6cec33bb
Clara Andrew <candrewwani@gmail.com>
2018-05-31 09:15:53 -0400

    websocket working

--- src/App.js
+++ src/App.js
@@ -1,39 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-// import ReconnectingWebSocket from 'reconnecting-websocket';
 import { WebSocketBridge } from 'django-channels'
 
 let quizName = 'testing1'
 
-// // window.location.pathname = '/quiz/' + quizName + '/';
-
-// let quizSocket = new WebSocket(`ws://127.0.0.1:8000/ws/quiz/${quizName}/`)
-
-
-// const scheme = window.location.protocol === 'https:' ? 'wss' : 'ws';
-
-// const base_url = `${scheme}://127.0.0.1:8000/api/ws/quiz/test`
-// let quizSocket = new ReconnectingWebSocket(base_url)
-
 const webSocketBridge = new WebSocketBridge();
-webSocketBridge.connect('ws://127.0.0.1:8000/ws/quiz/testing1/');
-webSocketBridge.listen(function(action, stream) {
-  console.log(action, stream);
-});
+// webSocketBridge.connect('ws://127.0.0.1:8000/ws/quiz/testing1/');
+// webSocketBridge.listen(function(action, stream) {
+//   console.log(action, stream);
+// });
 
-// quizSocket.onmessage = function(e) {
-//   let data = JSON.parse(e.data);
-//   let message = data['message'];
-//   console.log('Message from server', message)
-// }
-
-
-// quizSocket.onclose = function(e) {
-//     console.error('Quiz socket closed unexpectedly');
-//   }
+// webSocketBridge.onmessage = function(e) {
+//   var data = JSON.parse(e.data);
+//   var message = data['message'];
+//   console.log(message)
+// };
 class App extends Component {
   state = {
-    teams: []
+    teams: [], 
+    message: ''
   };
 
   // async componentDidMount() {
@@ -50,26 +35,47 @@ class App extends Component {
   // }
 
   async componentDidMount() {
-    // try {
-    //   quizSocket.onopen = () => {
-    //     quizSocket.send(JSON.stringify({
-    //       'message': "hello!"
-    //     }));
-    //   } 
-    // } catch(e) {
-    //   console.error(e)
-    // }
+    try {
+      // for deployment in local change to http://127.0.0.1:8000/api/
+      webSocketBridge.connect('ws://127.0.0.1:8000/ws/quiz/testing1/');
+      webSocketBridge.listen(function(action, stream) {
+        console.log(action, stream);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  messageChangeHandler = event => {
+    event.preventDefault();
+
+    const {name, value} = event.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  submitMessage = (event) => {
+    event.preventDefault();
+
+  
+    console.log('message being sent', this.state.message)
+    webSocketBridge.send(JSON.stringify({
+      'message': this.state.message
+    }));
   }
   render() {
     return (
 
   
       <div>
-        {this.state.teams.map(item => (
+        {/* {this.state.teams.map(item => (
           <div key={item.id}>
             <h1>{item.title}</h1>
           </div>
-        ))}
+        ))} */}
+        <input type="text" size="100" value={this.state.message} name='message' onChange={this.messageChangeHandler}/><br/>
+        <button onClick={this.submitMessage}>Send</button>
       </div>
 
     );
