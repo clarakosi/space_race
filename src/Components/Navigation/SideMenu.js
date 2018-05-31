199f8778644a8781183e42708ea008e365e1f8f2
Capril1042 <Caprilewis1042@gmail.com>
2018-05-31 13:51:50 -0400

    Side Menu Set up- needs to be properly linked to respective pages

--- src/Components/Navigation/SideMenu.js
+++ src/Components/Navigation/SideMenu.js
@@ -1,31 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Nav, NavItem, NavLink } from 'reactstrap';
 
-export default class Example extends React.Component {
-  render() {
-    return (
-      <div>
-        <p>List Based</p>
+//Basic Skeleton of SideMenu
+//TODO: update urls.includes() to respective pages
+// add dropdown to races link
+//figure out what schools links to
+
+
+const SideMenu = (props) => {
+    const url = window.location.href;
+    if(url.includes('/join')) {
+        return (
+            <div>
         <Nav vertical>
-          <NavItem>
-            <NavLink href="#">Link</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#">Link</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#">Another Link</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink disabled href="#">Disabled Link</NavLink>
-          </NavItem>
+          <NavLink href="/races">Races</NavLink> 
+          <NavLink href="/scoreboard">Scoreboard</NavLink> 
+          <NavLink href="/settings">Settings</NavLink> 
         </Nav>
-        <hr />
-        <p>Link based</p>
+      </div> )
+
+    } else if(url.includes('/race')) {
+        return (
+            <div>
         <Nav vertical>
-          <NavLink href="#">Link</NavLink> <NavLink href="#">Link</NavLink> <NavLink href="#">Another Link</NavLink> <NavLink disabled href="#">Disabled Link</NavLink>
+          <NavLink href="/races">Races</NavLink> 
+          <NavLink href="/scoreboard">Scoreboard</NavLink> 
+          <NavLink href="/settings">Settings</NavLink> 
         </Nav>
-      </div>
-    );
-  }
-}
+      </div>)
+
+    }else if(url.includes('/admindeliverypage')) {
+        return (
+             <div>
+        <Nav vertical>
+          <NavLink href="/schools">Schools</NavLink> 
+          <NavLink href="/scoreboard">Scoreboard</NavLink> 
+          <NavLink href="/settings">Settings</NavLink> 
+        </Nav>
+      </div>)
+
+    } else if(url.includes('/settings')) {
+        return (
+            <div>
+        <Nav vertical>
+          <NavLink href="/races">Races</NavLink> 
+          <NavLink href="/scoreboard">Scoreboard</NavLink> 
+          <NavLink href="/settings">Settings</NavLink> 
+        </Nav>
+      </div> )
+    } else {
+        return null;
+    }
+
+};
+
+export default SideMenu;
