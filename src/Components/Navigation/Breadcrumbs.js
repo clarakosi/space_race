544c56ad3f586261ca8147873903f89e65f43302
Capril1042 <Caprilewis1042@gmail.com>
2018-05-31 13:20:43 -0400

    Navigation files laid out, Breadcrumbs Working

---
+++ src/Components/Navigation/Breadcrumbs.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
+//Breadcrumbs are set up and working for Settings page
+//TODO: update the includes statments based on pages urls-
+//lines 9,18,& 27 will need to be updated
+
+const Breadcrumbs = (props) => {
+    const url = window.location.href;
+    if(url.includes('/join')) {
+        return (
+            <div>
+                <Breadcrumb tag="nav">
+                    <BreadcrumbItem tag="a" href="/">Home</BreadcrumbItem>
+                    <BreadcrumbItem active tag="span">Races</BreadcrumbItem>
+                </Breadcrumb>
+            </div> )
+
+    } else if(url.includes('/race')) {
+        return (
+            <div >
+                <Breadcrumb tag="nav">
+                    <BreadcrumbItem tag="a" href="/">Home</BreadcrumbItem>
+                    <BreadcrumbItem active tag="span">Race New</BreadcrumbItem>
+                </Breadcrumb>
+            </div> )
+
+    }else if(url.includes('/scoreboard')) {
+        return (
+            <div >
+                <Breadcrumb tag="nav">
+                    <BreadcrumbItem tag="a" href="/">Home</BreadcrumbItem>
+                    <BreadcrumbItem active tag="span">Scoreboard</BreadcrumbItem>
+                </Breadcrumb>
+            </div> )
+
+    } else if(url.includes('/settings')) {
+        return (
+            <div>
+                <Breadcrumb tag="nav">
+                    <BreadcrumbItem tag="a" href="/">Home</BreadcrumbItem>
+                    <BreadcrumbItem active tag="span">Settings</BreadcrumbItem>
+                </Breadcrumb>
+            </div> )
+    } else {
+        return null;
+    }
+
+};
+
+export default Breadcrumbs;
