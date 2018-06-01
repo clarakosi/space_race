915f7146f27418af4fa7a94282a5be632a2dddd8
Capril1042 <Caprilewis1042@gmail.com>
2018-05-31 21:32:51 -0400

    Updates SideMenu

--- src/Components/Navigation/SideMenu.js
+++ src/Components/Navigation/SideMenu.js
@@ -2,39 +2,39 @@ import React, { Component } from 'react';
 import { Nav, NavItem, NavLink } from 'reactstrap';
 
 //Basic Skeleton of SideMenu
-//TODO: update urls.includes() to respective pages
+//TODO: \
 // add dropdown to races link
 //figure out what schools links to
 
 
 const SideMenu = (props) => {
     const url = window.location.href;
-    if(url.includes('/join')) {
+    if(url.includes('/adminrace')) {
         return (
             <div>
         <Nav vertical>
-          <NavLink href="/races">Races</NavLink> 
-          <NavLink href="/scoreboard">Scoreboard</NavLink> 
+          <NavLink href="/adminrace">Races</NavLink> 
+          <NavLink href="/admindelivery">Scoreboard</NavLink> 
           <NavLink href="/settings">Settings</NavLink> 
         </Nav>
       </div> )
 
-    } else if(url.includes('/race')) {
+    } else if(url.includes('/createrace') || url.includes('/showrace')) {
         return (
             <div>
         <Nav vertical>
           <NavLink href="/races">Races</NavLink> 
-          <NavLink href="/scoreboard">Scoreboard</NavLink> 
+          <NavLink href="/admindelivery">Scoreboard</NavLink> 
           <NavLink href="/settings">Settings</NavLink> 
         </Nav>
       </div>)
 
     }else if(url.includes('/admindeliverypage')) {
         return (
-             <div>
+             <div className="Menu">
         <Nav vertical>
-          <NavLink href="/schools">Schools</NavLink> 
-          <NavLink href="/scoreboard">Scoreboard</NavLink> 
+          <NavLink href="/adminrace">Schools</NavLink> 
+          <NavLink href="/admindelivery">Scoreboard</NavLink> 
           <NavLink href="/settings">Settings</NavLink> 
         </Nav>
       </div>)
@@ -44,7 +44,7 @@ const SideMenu = (props) => {
             <div className="Menu" >
         <Nav vertical>
           <NavLink href="/races">Races</NavLink> 
-          <NavLink href="/scoreboard">Scoreboard</NavLink> 
+          <NavLink href="/admindelivery">Scoreboard</NavLink> 
           <NavLink href="/settings">Settings</NavLink> 
         </Nav>
       </div> )
