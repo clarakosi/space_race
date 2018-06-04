ce97db7aafcf3014548fed220d30f113637b5a67
FJF616 <franzjfernando@hotmail.com>
2018-06-03 23:53:12 -0700

    adds corrected random team generator function, adds extra random color generator as well. adds random generator container file for all necessary action creator helpers. These are needed in order to keep the reducer as a pure function

--- src/Components/CardViews/Refactored/RandomGenerator.js
+++ src/Components/CardViews/Refactored/RandomGenerator.js
@@ -27,7 +27,7 @@ export function Randomize() {
     return `${rando(adjectives)}-${rando(adjectives)}-${rando(nouns)}`;
   }
 /*****produces an array of length n, of colors */
-export function RandomColor(n) {
+export function RandomColor(n=1) {
     const ColorArray = ['#FFFF00', '#FFA500', '#FF1493', '#FF0000', '#00FF00', '#008000', '#40E0D0', '#4682B4', '#B0C4DE', '#0000FF', '#A52A2A', '#C0C0C0', '#808080', '#000000'];
     let tempArray = [];
     for (let i = 0; i < ColorArray.length; i++ ) {
@@ -42,7 +42,7 @@ export function RandomColor(n) {
     return choiceArray;
 }
 /****produces an array of length n,  of random team names. */
-export function RandomTeams(n) {
+export function RandomTeams(n=1) {
   teamsArray=[];
   for (let i =0; i < n; i++) {
     let team = Randomize();
@@ -51,4 +51,41 @@ export function RandomTeams(n) {
   return teamsArray;
 } 
 
- 
+/**
+ * this part is tricky. previously, the action was set up incorrectly, because it calls for an impure function in the reducer.  Albeit the random function to shuffle the teams was fine on its own, but since I have to 
+ * redo it, Ive expanded the random function to also randomize colors as well, and the random function will not modify its arguments, however, since it still calls for a random action, it is 
+ * still impure.  NOTE:  to get around this I will pass the random return values as arguments to the action creator.
+ * 
+ * 
+ * (in the actions file we will have this action creator)
+ * 
+* ex:     function RandomTeamAction(team) {
+ *            return {
+ *              type: 'RANDOM_TEAM_ACTION',
+ *              payload: team
+ *                     
+ *            }
+ *        }
+ * **************************************************************************
+ * 
+ * outside of this elsewhere in the code we will do this:
+ *        
+ *        import RandomTeamAction from './Actions';
+ *        import RandomTeams from './RandomGenerator';
+ * 
+ *        // somewhere in the component code we will dispatch the action like so:
+ *        // NOTE: as usual we will probably need the context 'this', or 'this.props'
+ *        
+ *          RandomTeamAction(RandomTeams());
+ * 
+ * **************************************************************************
+ *  Since the action creator is being passed the portion that caused the reducer to 
+ *  originally be impure, we no longer get undesired side effects.  The randomizing logic
+ *  is implemented outside of the reducer through the function below and finally handled by
+ *  the action creator inside of the component.
+ * 
+ *      function RandomTeams();  which is defined above, so all that is needed is that both RandomTeamAction and RandomTeams be imported 
+ *                               into the component that needs to use them.  This same rule applies for RandomColor.
+ *     
+ * 
+ */
