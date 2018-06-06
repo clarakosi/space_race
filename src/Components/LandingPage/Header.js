542305497fa9f470a0c1b43cdf0f20d933e783df
FJF616 <franzjfernando@hotmail.com>
2018-06-05 20:07:20 -0700

    added authenticated routes, and buy now / sign up modal along with appropriat ereducers and actions

--- src/Components/LandingPage/Header.js
+++ src/Components/LandingPage/Header.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler,
   NavItem, NavLink } from 'reactstrap';
 import { Textfit } from 'react-textfit';
+import BuyNowModal from '../Modal/BuyNowModal';
  
 class Header extends Component {
   constructor(props) {
@@ -16,6 +17,46 @@ class Header extends Component {
     this.setState({
       isOpen: !this.state.isOpen
     });
+  }
+  toggleSignUpModal(){
+    this.props.signUpToggle(this.props.modal);
+  }
+
+// if the user has paid for the product, the menu will allow him to go to Home or Default Page (these routes are authenticated routes), if the user has just 
+//signed up, then the billing modal will come up along with the sign up button. also clicking the sign up button will first bring up the payment plan.  
+  getLinks() {
+    if (this.props.authenticated) {
+      /* Signed In */
+      const _l = [
+        <NavItem key={1}>
+          <NavLink tag={Link} to="/"  >Home</NavLink>
+        </NavItem>,
+        <NavItem key={2}>
+          <NavLink tag={Link} to="/Dashboard" >Dashboard</NavLink>
+        </NavItem>
+      ];
+      if(this.props.registered){
+        _l.push(
+          <NavItem key={3}>
+            <NavLink tag={Link} to="/Billing"  >Billing</NavLink>
+          </NavItem>);
+      } else {
+        _l.push(
+          <NavItem key={3}>
+            <a className="nav-link" onClick={this.toggleSignUpModal.bind(this)} style={{cursor:'pointer'}}>Sign-Up</a>
+          </NavItem>);
+      }
+      return _l;
+    }
+    return [
+      /* Signed out.. */
+      <NavItem key={1}>
+        <NavLink tag={Link} to="/" className="" >Home</NavLink>
+      </NavItem>,
+      <NavItem key={2}>
+        <InstaLogin />
+      </NavItem>
+    ];
   }
 
   render() {
@@ -35,9 +76,7 @@ class Header extends Component {
           </NavbarBrand>
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="#SignUp">SignUp</NavLink>
-              </NavItem>
+                {this.getLinks()}
               <NavItem>
                 <NavLink href="#SignIn">SignIn</NavLink>
               </NavItem>
@@ -47,6 +86,7 @@ class Header extends Component {
             </Nav>
           </Collapse>
         </Navbar>
+        <BuyNowModal />
       </div>
     );
   }
