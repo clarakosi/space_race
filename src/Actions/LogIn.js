69b2cb89aca179dbf46c35c52640c9793cd692c3
Clara Andrew <candrewwani@gmail.com>
2018-06-06 00:02:42 -0400

    sign out connected & routed

--- src/Actions/LogIn.js
+++ src/Actions/LogIn.js
@@ -4,12 +4,16 @@ export const LOGGINGIN = 'LOGGINGIN';
 export const LOGGEDIN = 'LOGGEDIN';
 export const ERROR = 'ERROR';
 export const SIGNINGUP = 'SIGNINGUP';
+export const SIGNINGOUT = 'SIGNINGOUT';
+export const SIGNEDOUT = 'SIGNEDOUT';
 
 
-// TODO: change url for deployment
+// TODO: change urls for deployment
 const url = 'http://127.0.0.1:8000/api/v1/rest-auth/login/'
 
 const signUpUrl = 'http://127.0.0.1:8000/api/v1/rest-auth/registration/';
+
+const redirectURL = 'http://localhost:3000/'
 
 
 export const loggingIn = (data) => {
@@ -39,5 +43,15 @@ export const signingUp = (data) => {
       .catch(error => {
         dispatch({type: ERROR, payload: error})
       })
+  }
+}
+
+
+export const signOut = () => {
+  return dispatch => {
+    dispatch({type: SIGNINGOUT})
+    window.localStorage.clear();
+    dispatch({type: SIGNEDOUT})
+    document.location.replace(redirectURL)   
   }
 }
--- src/Reducers/LogIn.js
+++ src/Reducers/LogIn.js
@@ -1,4 +1,11 @@
-import {LOGGINGIN, LOGGEDIN, ERROR, SIGNINGUP} from '../Actions/LogIn';
+import {
+  LOGGINGIN,
+  LOGGEDIN,
+  ERROR,
+  SIGNINGUP,
+  SIGNEDOUT,
+  SIGNINGOUT
+} from "../Actions/LogIn";
 
 const initialState = {
   loggedIn: false,
@@ -6,22 +13,33 @@ const initialState = {
   signingUp: false,
   user: null,
   error: null,
-}
+  signingOut: false,
+  signedOut: false
+};
 
-
-const LogInReducer = (state=initialState, action) => {
-  switch(action.type) {
+const LogInReducer = (state = initialState, action) => {
+  switch (action.type) {
     case LOGGINGIN:
-      return {...state, loggingIn: true}
+      return { ...state, loggingIn: true };
     case LOGGEDIN:
-      return {...state, loggedIn: true, loggingIn: false, signingUp: false, user: action.payload}
+      return {
+        ...state,
+        loggedIn: true,
+        loggingIn: false,
+        signingUp: false,
+        user: action.payload
+      };
     case SIGNINGUP:
-      return {...state, signingUp: true}
+      return { ...state, signingUp: true };
+    case SIGNINGOUT:
+      return {...state, signingOut: true};
+    case SIGNEDOUT:
+      return {...state, signedOut: true, loggedIn: false, signingOut: false};
     case ERROR:
-      return {...state, loggingIn: false, error: action.payload}
+      return { ...state, loggingIn: false, error: action.payload };
     default:
       return state;
   }
-}
+};
 
 export default LogInReducer;
